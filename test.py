3c498a60e632112c0889d9df6c0064646f889273
Gianmarco Calbi <calbigianmarco@gmail.com>
2017-07-26 18:13:16 +0200

    Added all Stats files

--- test.py
+++ test.py
@@ -5,3 +5,17 @@ import traceback
 from time import sleep
 import numpy as np
 import pylab
+
+ga1 = []
+ga2 = []
+opt = []
+
+pylab.figure(1)
+pylab.title('Mean waiting time')
+pylab.xlabel('Time (HMS)')
+pylab.ylabel('Time (sec)')
+pylab.plot(ga1, c='r')
+pylab.plot(ga2,c='g')
+pylab.plot(opt, c = 'b')
+
+pylab.show()
